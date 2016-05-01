88c35bc23ddc4ede96d4c20a706fe79cb89cb1ff
JonLuca DeCaro <jdecaro@usc.edu>
2016-04-30 18:01:42 -0700

    Regex implementation to find all imgur.com/gallery/ links

--- content_script.js
+++ content_script.js
@@ -1,5 +1,5 @@
 $(document).on("click", "a", function(){
     var link_target = $(this).attr('href');
-    var converted_url = link_target.replace(www\.\imgur.com\/gallery\/, i\.imgur\.com\/$1);
+    var converted_url = link_target.replace((www\.)?imgur.com\/gallery\/, i\.imgur\.com\/);
     $(this).attr('href', converted_url);
 });
