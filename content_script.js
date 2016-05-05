6c7f0c602effd6018ddc5f334040301ec44446f2
JonLuca DeCaro <jdecaro@usc.edu>
2016-05-05 14:43:07 -0700

    Updated icon

--- content_script.js
+++ content_script.js
@@ -1,12 +1,11 @@
 var el = document.getElementsByTagName("a");
-var j = 0;
-for(j=0;j<el.length;j++){
+for(var j=0;j<el.length;j++){
 	if((el[j].href).indexOf("imgur.com/a/") != -1){
 		continue;
 	}
 	if((el[j].href).indexOf("imgur.com") != -1){
     	el[j].href = el[j].href.replace(/(www\.)?imgur.com\/gallery\//, "i.imgur.com/");
-    	if((el[j].href).indexOf("gif") == -1 && el[j].href.indexOf("/a/")){
+    	if((el[j].href).indexOf("gif") == -1 && el[j].href.indexOf("/a/") == -1){
     		el[j].href += .jpg;
     	}
     }
