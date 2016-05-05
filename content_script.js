db58b078be0c8fe4a3d384f1d8e1912c1eef696f
JonLuca DeCaro <jdecaro@usc.edu>
2016-05-05 14:34:43 -0700

    Added album checks + video link checks

--- content_script.js
+++ content_script.js
@@ -1,5 +1,13 @@
 var el = document.getElementsByTagName("a");
 var j = 0;
 for(j=0;j<el.length;j++){
-    el[j].href = el[j].href.replace(/(www\.)?imgur.com\/gallery\//, "i.imgur.com/") + '.jpg';
+	if((el[j].href).indexOf("imgur.com/a/") != -1){
+		continue;
+	}
+	if((el[j].href).indexOf("imgur.com") != -1){
+    	el[j].href = el[j].href.replace(/(www\.)?imgur.com\/gallery\//, "i.imgur.com/");
+    	if((el[j].href).indexOf("gif") == -1 && el[j].href.indexOf("/a/")){
+    		el[j].href += .jpg;
+    	}
+    }
 }
